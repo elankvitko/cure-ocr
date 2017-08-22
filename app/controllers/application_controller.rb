7e0ef59215f5dc83eb21a5a50a9cc26154f5c538
Elan Kvitko <elan@idknyc.com>
2017-08-22 09:44:54 -0400

    test fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def upload_pic( image )
-    image = Cloudinary::Uploader.upload( Rails.root.join( 'public', 'uploads', image ), api_key: "875836294774873", api_secret: "2ODjxbzdrSstbEOAphaC0hrZI1g", cloud_name: "dsqbi2aqd" )
+    image = Cloudinary::Uploader.upload( Rails.root.join( 'public', 'uploads', image ), api_key: ENV[ "CLOUDINARY_API_KEY" ], api_secret: ENV[ "CLOUDINARY_API_SECRET" ], cloud_name: ENV[ "CLOUDINARY_CLOUD_NAME" ] )
     image[ 'url' ]
   end
 
   def ocr_helper
-    OcrSpace::Resource.new(apikey: "285458c2f288957")
+    OcrSpace::Resource.new( apikey: ENV[ "OCR_HELPER_API_KEY" ] )
   end
 end
